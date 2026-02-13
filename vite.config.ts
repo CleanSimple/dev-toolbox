ebb9c5538ce1c89bb898500fc06ccf40e4980a05
Nourz1234 <104628709+Nourz1234@users.noreply.github.com>
2026-02-13 14:42:05 +0200

    remove postcss

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 import lucidePreprocess from "vite-plugin-lucide-preprocess";
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-  plugins: [solid(), lucidePreprocess()],
+  plugins: [solid(), lucidePreprocess(), tailwindcss()],
 })

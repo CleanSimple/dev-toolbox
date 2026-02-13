4ea0c2a2effa530f8345cf85cf22b9cea1809473
Nourz1234 <104628709+Nourz1234@users.noreply.github.com>
2026-02-13 15:16:50 +0200

    stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 import lucidePreprocess from "vite-plugin-lucide-preprocess";
-
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({

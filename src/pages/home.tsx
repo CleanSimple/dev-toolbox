11b84813138a829990588cef6a1f97f3aad9b903
Nourz1234 <104628709+Nourz1234@users.noreply.github.com>
2026-06-12 18:33:01 +0300

    remove old ui

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,61 +1,11 @@
-import { For } from "solid-js";
-import SourceInput from "../components/SourceInput";
-import AddOperation from "../components/AddOperation";
-import Output from "../components/Output";
 import type { Component } from "solid-js";
-import type { DataFormat, Operation } from "../operations";
-import { createOperationPipeline } from "../hooks/createOperationPipeline";
 
 
 const Home: Component = () => {
-    const operationsPipeline = createOperationPipeline();
-
-    const handleSourceChange = (input: string, format: DataFormat) => {
-        operationsPipeline.setInput({ value: input, format });
-    };
-
-    const handleAddOperation = (operation: Operation) => {
-        operationsPipeline.addOperation(operation);
-    };
-
     return (
         <div class="min-h-screen p-6">
             <div class="max-w-4xl mx-auto">
-                <h1 class="text-3xl font-bold text-main mb-6">
-                    Dev Toolbox
-                </h1>
 
-                {/* Input Section */}
-                <div class="flex flex-col gap-4 mb-6">
-                    <h2 class="text-xl font-semibold text-main">
-                        Input
-                    </h2>
-                    <SourceInput
-                        onValueChange={handleSourceChange}
-                    />
-                    <AddOperation
-                        onAddOperation={handleAddOperation}
-                    />
-                </div>
-
-                {/* Output Section */}
-                <For each={operationsPipeline.outputs()}>
-                    {(outputChain) => (
-                        <For each={outputChain}>
-                            {({ operationName, result, error }) => (
-                                <div class="flex flex-col gap-4">
-                                    <h2 class="text-xl font-semibold text-main">
-                                        Output of {operationName}
-                                    </h2>
-                                    <Output
-                                        value={result()}
-                                        error={error()}
-                                    />
-                                </div>
-                            )}
-                        </For>
-                    )}
-                </For>
             </div>
         </div>
     )

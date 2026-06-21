4a24aadc61f0eabe77e532723d368bb226a3b0c5
Nourz1234 <104628709+Nourz1234@users.noreply.github.com>
2026-06-21 14:28:46 +0300

    add support for color schemes

--- src/utils/styling.ts
+++ src/utils/styling.ts
@@ -1,13 +1,17 @@
 
-interface MakeStyleOptions<TVariant, TSize> {
+interface MakeStyleOptions<TVariant, TColor, TSize> {
     variant?: TVariant
+    color?: TColor
     size?: TSize
 }
 
-export function makeStyle<TVariant, TSize>(props: MakeStyleOptions<TVariant, TSize>, defaults?: MakeStyleOptions<TVariant, TSize>) {
+export function makeStyle<TVariant, TColor, TSize>(props: MakeStyleOptions<TVariant, TColor, TSize>, defaults?: MakeStyleOptions<TVariant, TColor, TSize>) {
     return {
         variant(variant: TVariant, style: string) {
             return variant == (props.variant ?? defaults?.variant) ? style : null;
+        },
+        color(color: TColor, style: string) {
+            return color == (props.color ?? defaults?.variant) ? style : null;
         },
         size(size: TSize, style: string) {
             return size == (props.size ?? defaults?.size) ? style : null;
